fad04f3a21180530a58f27799d576c76fb6ca145
DanielTAntonio <danieltomantonio@gmail.com>
2021-09-25 04:06:28 -0700

    Changed up whole Structs, revamping whole functionality

--- SharedWallet.sol
+++ SharedWallet.sol
@@ -1,60 +1,39 @@
 pragma solidity ^0.8.7;
 
 contract SharedWallet {
-    address owner;
+   address owner;
 
-    struct Withdraw {
-        uint amount;
-        uint timestamp;
-    }
+   struct Request {
+       address req;
+       uint amount;
+       uint timestamp;
+   }
 
-    struct Payment {
-        uint amount;
-        uint timestamp;
-    }
+   struct WithdrawHistory {
+       uint amount;
+       uint timestamp;
+   }
 
-    struct Request {
-        address payee;
-        uint amount;
-        uint timestamp;
-    }
+   struct Withdraws {
+       uint totalRequests;
+       uint activeRequests;
+       mapping(uint => WithdrawHistory) withdrawHistory;
+   }
 
-    struct Balance {
-        uint totalBalance;
-        uint numTransactions;
-        uint withdrawRequests;
-        mapping(uint => Withdraw) withdraws;
-        mapping(uint => Payment) payments;
-        mapping(uint => Request) requests;
-    }
+   struct Payments {
+       address req;
+       mapping(address => Withdraws) withdraws;
+   }
 
-    constructor() {
-        owner = msg.sender;
-    }
+   struct Wallet {
+       uint totalBalance;
+       uint numTransactions;
+       uint withdrawReqs;
+       mapping(uint => Request) requests;
+       mapping(uint => Payments) payments;
+   }
 
-    mapping(address => Balance) public wallet;
-
-    function depositMoney() public payable {
-        require(msg.sender == owner, "You are not the owner");
-        wallet[owner].totalBalance += msg.value;
-        wallet[owner].numTransactions++;
-        Payment memory payment = Payment(msg.value, block.timestamp);
-        wallet[owner].payments[wallet[owner].numTransactions] = payment;
-    }
-
-    function showTotalRequests() public view returns (uint) {
-        require(msg.sender == owner, "You are not the owner");
-        return wallet[msg.sender].withdrawRequests;
-    }
-
-    function showWithdrawRequest(uint _request) public view returns(Request memory) {
-        require(msg.sender == owner, "You are not the owner");
-        return wallet[owner].requests[_request];
-    }
-
-    function requestWithdraw() public {
-        Request memory request = Request(msg.sender, 1 ether, block.timestamp);
-        wallet[owner].withdrawRequests++;
-        wallet[owner].requests[wallet[owner].withdrawRequests] = request;
-    }
+   constructor() {
+       owner = msg.sender;
+   }
 }
