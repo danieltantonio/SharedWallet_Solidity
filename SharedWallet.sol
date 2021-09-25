17f8cf2a9c858f44ae2849b260cbbe9bc9b0827d
DanielTAntonio <danieltomantonio@gmail.com>
2021-09-24 17:55:40 -0700

    Renamed vars, added show functions, made request functionality

--- SharedWallet.sol
+++ SharedWallet.sol
@@ -13,24 +13,48 @@ contract SharedWallet {
         uint timestamp;
     }
 
+    struct Request {
+        address payee;
+        uint amount;
+        uint timestamp;
+    }
+
     struct Balance {
         uint totalBalance;
-        uint numPayments;
+        uint numTransactions;
+        uint withdrawRequests;
         mapping(uint => Withdraw) withdraws;
         mapping(uint => Payment) payments;
+        mapping(uint => Request) requests;
     }
 
     constructor() {
         owner = msg.sender;
     }
 
-    mapping(address => Balance) public totalBalance;
+    mapping(address => Balance) public wallet;
 
     function depositMoney(uint _amount) public payable {
         require(msg.sender == owner, "You are not the owner");
-        totalBalance[owner].totalBalance += _amount;
-        totalBalance[owner].numPayments++;
+        wallet[owner].totalBalance += _amount;
+        wallet[owner].numTransactions++;
         Payment memory payment = Payment(_amount, block.timestamp);
-        totalBalance[owner].payments[totalBalance[owner].numPayments] = payment;
+        wallet[owner].payments[wallet[owner].numTransactions] = payment;
+    }
+
+    function showTotalRequests() public view returns (uint) {
+        require(msg.sender == owner, "You are not the owner");
+        return wallet[msg.sender].withdrawRequests;
+    }
+
+    function showWithdrawRequest(uint _request) public view returns(Request memory) {
+        require(msg.sender == owner, "You are not the owner");
+        return wallet[owner].requests[_request];
+    }
+
+    function requestWithdraw(uint _amount) public {
+        Request memory request = Request(msg.sender, _amount, block.timestamp);
+        wallet[owner].withdrawRequests++;
+        wallet[owner].requests[wallet[owner].withdrawRequests] = request;
     }
 }
